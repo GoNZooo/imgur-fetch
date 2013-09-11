edcdb7ca0c088ca489135aa722480b384231cfad
Rickard Andersson <rickard.m.andersson@gmail.com>
2013-09-11 22:07:52 +0200

    Moved the code for processing the command line flags out to a separate function so we can get a cleaner main module.

--- fetcher.rkt
+++ fetcher.rkt
@@ -7,11 +7,10 @@
 
 ; Originally I'd hoped to use Racket's HTML parser to
 ; clean things up a little in this one, but it's not happening.
-; Looking at the docs (http://docs.racket-lang.org/html/) it
+; Looking at the docs (http://docs.racket-lang.org/html/), it
 ; should be easy for anyone and everyone to see why it's not
 ; going to make anything cleaner and more elegant.
 
-; Function that grabs the source for a site corresponding to the supplied url.
 (define (get-source url)
   (call/input-url (string->url url) get-pure-port port->string))
 
@@ -21,6 +20,8 @@
    src
    #:match-select cadr))
 
+; Extract author name from source HTML.
+; If not possible, return #f
 (define (get-author-name src)
   (let ([result (regexp-match
                  #rx"By <a href=\".*?\">([a-zA-Z0-9-]*?)</a>"
@@ -58,7 +59,7 @@
 (define (get-album-name album-url)
   (last (string-split album-url "/")))
 
-(module+ main
+(define (assemble-command-line-flags)
   ; Set our current path to the base path
   ; in path-config.rkt, change if -d flag present
   (define path (get-download-base-path))
@@ -76,6 +77,10 @@
      
      #:args (album-url) ; general arg, always supplied and without flag
      album-url)) ; always return album-url
+  (values path album-url))
+  
+(module+ main
+  (define-values (base-path album-url) (assemble-command-line-flags))
   
   (let* ([src (get-source album-url)]
          [urls (extract-full-res-urls src)]
@@ -84,7 +89,6 @@
 
     (when (false? author)
       (set! author ""))
-    (let ([final-download-path (download-images urls author album-name path)])
+    (let ([final-download-path (download-images urls author album-name base-path)])
       (printf "Downloaded album '~a' to ~a~n"
               (get-album-name album-url) final-download-path))))
-             
