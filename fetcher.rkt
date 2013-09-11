d89a897cde709e561c9f0f4eef2561d8a9388fb5
GoNZooo <rickard.m.andersson@gmail.com>
2013-09-11 15:37:58 +0200

    Fixed main module up a bit, moved mkdir and download-images functions instead.

--- fetcher.rkt
+++ fetcher.rkt
@@ -3,28 +3,41 @@
 (require net/url
          racket/cmdline)
 
+; Originally I'd hoped to use Racket's HTML parser to
+; clean things up a little in this one, but it's not happening.
+; Looking at the docs (http://docs.racket-lang.org/html/) it
+; should be easy for anyone and everyone to see why it's not
+; going to make anything cleaner and more elegant.
+
 ; Function that grabs the source for a site corresponding to the supplied url.
 (define (get-source url)
   (call/input-url (string->url url) get-pure-port port->string))
 
-(define (source->source-file source path)
-  (call-with-output-file path (lambda (output-port) (write-string source output-port)) #:exists 'replace))
-
-(define (url->source-file url path)
-  (source->source-file (get-source url) path))
-
 (define (download-file url [path ""])
-  (let ([file-bytes (call/input-url (string->url url) get-pure-port port->bytes)]
-        [filename (last (string-split url "/"))])
-    (if (equal? filename " ")
-        #f
-        (call-with-output-file (string-append path "/" filename) (lambda (output-port) (write-bytes file-bytes output-port)) #:exists 'replace))))
+  (define (get-file-bytes)
+    (call/input-url (string->url url) get-pure-port port->bytes))
+  (define filename (last (string-split url "/")))
+  
+  (if (equal? filename " ")
+      #f
+      (call-with-output-file (string-append path "/" filename)
+        (lambda (output-port) (write-bytes file-bytes output-port))
+        #:exists 'replace))))
 
 (define (extract-full-res-urls src)
   (regexp-match* #rx"href=\"([a-zA-Z0-9:/\\.]*?)\" target=\"_blank\">View full resolution</a>" src #:match-select cadr))
 
 (define (get-author-name src)
   (second (regexp-match #rx"By <a href=\".*?\">([a-zA-Z0-9-]*?)</a>" src)))
+
+(define (create-author-directory author)
+  (when (not (directory-exists? author))
+    (make-directory author)))
+
+(define (download-images image-urls author)
+  (for-each (lambda (image-url)
+              (download-file image-url author))
+            image-urls))
 
 (module+ main
 
@@ -34,9 +47,5 @@
                 [urls (extract-full-res-urls src)]
                 [author (get-author-name src)])
 
-           (when (not (directory-exists? author))
-             (make-directory author))
-           
-           (for-each (lambda (image-url)
-                       (download-file image-url author))
-                     urls)))
+           (create-author-directory author)
+           (download-images urls author)))
